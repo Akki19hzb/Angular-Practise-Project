e90392560d640192ae530add7f136c9047f3ba7e
Akki19hzb <getakash.verma19@gmail.com>
2019-01-13 16:19:45 +0530

    @ the end of Module 3.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from './../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[] = [
+  // tslint:disable-next-line:max-line-length
+  new Recipe('Test Recipe', 'This is Testing Recipe',
+  'https://images.pexels.com/photos/748027/pexels-photo-748027.jpeg?cs=srgb&dl=cooking-fish-peanuts-748027.jpg&fm=jpg'),
+  new Recipe('Test Recipe', 'This is Testing Recipe',
+  'https://images.pexels.com/photos/748027/pexels-photo-748027.jpeg?cs=srgb&dl=cooking-fish-peanuts-748027.jpg&fm=jpg'),
+];
   constructor() { }
 
   ngOnInit() {
